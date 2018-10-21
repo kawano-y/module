798bbcc8eb4e0925a738c10fc10c6741007fc0ac
yoshinari kawano <kwn5212@yahoo.co.jp>
2018-10-21 11:20:10 +0900

    srp

--- src/main/java/com/module/bilder/json/PersonBilder.java
+++ src/main/java/com/module/bilder/json/PersonBilder.java
@@ -5,7 +5,7 @@ import com.module.bilder.Person;
 public class PersonBilder extends AbstractJsonBuilder<Person> {
 
 	private Person person;
-
+	
 	public PersonBilder() {
 		super();
 		this.person = new Person();
@@ -16,22 +16,33 @@ public class PersonBilder extends AbstractJsonBuilder<Person> {
 		return this.person;
 	}
 
-	public PersonBilder person(String name, String jyusyo, Integer age, IStrongBilder strongBuilder){
+	public PersonBilder person(String name, String jyusyo, Integer age, IStrong strong){
 		person.setName(name);
 		person.setAge(age);
-		person.setStlonger(strongBuilder.strong());
+		person.setStlonger(strong.strong());
 		return this;
 	}
 
-	interface IStrongBilder {
+	interface IStrong {
 		String strong();
 	}
 
-	public class StrongBilder implements IStrongBilder {
+	public static class Strong implements IStrong {
 
+		private String strong;
+
+		public Strong(String strong) {
+			super();
+			this.strong = strong;
+		}
+
+		public Strong(Integer strong) {
+			super();
+			this.strong = strong.toString();
+		}
 		@Override
 		public String strong() {
-			return "めっちゃ強い";
+			return strong;
 		}
 	}
 }
--- src/test/java/com/module/bilder/json/PersonBilderTest.java
+++ src/test/java/com/module/bilder/json/PersonBilderTest.java
@@ -6,15 +6,26 @@ import static org.junit.Assert.assertThat;
 import org.junit.Test;
 
 import com.module.bilder.Person;
+import com.module.bilder.json.PersonBilder.Strong;
 
 public class PersonBilderTest {
 
 	@Test
-	public void test() {
+	public void personbild() {
 		PersonBilder p = new PersonBilder();
-		Person pp = p.person("k", "東京都", 30, new StrongBilder("めっちゃ強い")).bild();
+		Person pp = p.person("k", "東京都", 30, new Strong("めっちゃ強い")).bild();
 		assertThat(pp.getName(), is("k"));
 		assertThat(pp.getAge(), is(30));
 		assertThat(pp.getStlonger(), is("めっちゃ強い"));
+	}
+
+	@Test
+	public void toIntStrong() {
+		PersonBilder p = new PersonBilder();
+		Person pp = p.person("k", "東京都", 30, new Strong(10_000)).bild();
+		assertThat(pp.getName(), is("k"));
+		
+		assertThat(pp.getAge(), is(30));
+		assertThat(pp.getStlonger(), is("10000"));
 	}
 }
